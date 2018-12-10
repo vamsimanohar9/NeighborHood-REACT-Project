ef2dd8d6588a3d6d1af25e5055f29611cf23f15b
Vamsi manohar <vamsimonohar@gmail.com>
2018-12-10 17:09:49 +0530

    location maker info window working fine

--- src/component/Map.js
+++ src/component/Map.js
@@ -11,10 +11,9 @@ import {
   const MyMapComponent = withScriptjs(
     withGoogleMap(props => (
       <GoogleMap
-        defaultZoom={8}
+        defaultZoom={8}// zoom value of map
         zoom={props.zoom}
-        defaultCenter={{ lat: 17.728663, lng: 83.235769 }}
-         //center={props.center}
+        defaultCenter={{ lat: 17.728663, lng: 83.235769 }} //visakhapatnam center coordinates
       >
       
        {props.markers &&
@@ -25,8 +24,8 @@ import {
                 venue => venue.id === marker.id);
               return ( 
        <Marker key={idx} 
-       position={{ lat: marker.lat, lng: marker.lng }} 
-       onClick={() => props.handleMarkerClick(marker)}
+       position={{ lat: marker.lat, lng: marker.lng }} //marker position
+       onClick={() => props.handleMarkerClick(marker)} //handling onclick
        animation={
         marker.isOpen === true
             ? window.google.maps.Animation.BOUNCE
@@ -34,19 +33,20 @@ import {
     }
        >
 
-       {marker.isOpen && 
-       venueInfo.bestPhoto && ( 
-       <InfoWindow>
-           <React.Fragment>
-               <img 
-               src={`${venueInfo.bestPhoto.prefix}200x200${
-                   venueInfo.bestPhoto.suffix}`}
-                   alt={"venue pics"}
-                   
-                   />
-        <p>{venueInfo.name}</p>
-        </React.Fragment>
-       </InfoWindow>
+       {marker.isOpen && ( 
+        <InfoWindow>
+    <div>
+      <p>{venueInfo.name}</p>
+       {venueInfo.bestPhoto && (
+         <React.Fragment>
+         <img src={`${venueInfo.bestPhoto.prefix}200x200$
+         {venueInfo.bestPhoto.suffix}`} 
+         alt={"Venue pics"}/>
+       </React.Fragment>
+                )}
+             <p>{venueInfo.location.address}</p>
+          </div>
+         </InfoWindow>
        )}
        </Marker>
        )})}
@@ -59,9 +59,9 @@ import {
           return(
             <MyMapComponent
              {...this.props}
-            googleMapURL="https://maps.googleapis.com/maps/api/js?v=3.exp&key=AIzaSyDW0MyJNDpQxRw_HIF0PRwSE8a3aEoAIcY"
+            googleMapURL="https://maps.googleapis.com/maps/api/js?v=3.exp&key=AIzaSyDW0MyJNDpQxRw_HIF0PRwSE8a3aEoAIcY" //google maps api key
             loadingElement={<div style={{ height: `100%` }} />}
-            containerElement={<div style={{ height: `100%`, width: `75%` }} />}
+            containerElement={<div style={{ height: `100%`, width: `75%` }} />}  //map properties
             mapElement={<div style={{ height: `100%` }} />}
           />
           )
